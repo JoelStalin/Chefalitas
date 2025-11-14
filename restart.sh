a5f881ccb4ef3842a72b4771ae3ff9468de083b4
Joel Stalin Martinez Espinal <ing.joelstalinmartinez@gmail.com>
2025-11-14 01:53:43 -0400

    mejor dyploy

--- restart.sh
+++ restart.sh
@@ -1,7 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+echo ">> Bajando contenedores..."
 docker compose down
-docker compose up  -d
-docker exec -it -u root odoo18-odoo-1 bash -c "
-  cd /mnt/extra-addons &&
+
+echo ">> Levantando contenedores en segundo plano..."
+docker compose up -d
+
+echo ">> Instalando dependencias en /mnt/extra-addons dentro del contenedor odoo18-odoo-1..."
+docker exec -u root odoo18-odoo-1 bash -c "
+  cd /mnt/extra-addons && \
   pip3 install --break-system-packages -r requirements.txt
 "
+
+echo ">> Reiniciando servicios de Docker Compose..."
 docker compose restart
+
+echo '>> Todo OK ✅'
