960706031956767c7a6845ac516d657f53c3ea1c
Joel Stalin Martinez Espinal <ing.joelstalinmartinez@gmail.com>
2026-01-09 20:11:04 -0400

    fix

--- custom_addons/pos_client_printer/models/__init__.py
+++
@@ -1,4 +0,0 @@
-from . import pos_client_printer
-from . import pos_client_self_order_terminal
-from . import pos_config
-from . import pos_order
--- custom_addons/pos_client_printer/models/pos_order.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models
-
-
-class PosOrder(models.Model):
-    _inherit = 'pos.order'
-
-    is_printed = fields.Boolean(default=False)
