4834cfb087326bc568d91e54c8a2376ac1b3f7fc
Joel Stalin Martinez Espinal <ing.joelstalinmartinez@gmail.com>
2026-02-07 20:57:04 -0400

    fix printer settings in POS config settings view

--- addons/pos_printing_suite/models/res_config_settings.py
+++ addons/pos_printing_suite/models/res_config_settings.py
@@ -5,6 +5,10 @@ from odoo import fields, models
 class ResConfigSettings(models.TransientModel):
     _inherit = "res.config.settings"
 
-    # Placeholder for global printing suite defaults if needed later.
-    # Legacy pos_any_printer_local used config_parameter; we keep config on pos.config only.
-    pass
+    printing_mode = fields.Selection(related="pos_config_id.printing_mode", readonly=False)
+    local_printer_cashier_name = fields.Char(related="pos_config_id.local_printer_cashier_name", readonly=False)
+    local_printer_kitchen_name = fields.Char(related="pos_config_id.local_printer_kitchen_name", readonly=False)
+    local_agent_host = fields.Char(related="pos_config_id.local_agent_host", readonly=False)
+    local_agent_port = fields.Integer(related="pos_config_id.local_agent_port", readonly=False)
+    any_printer_ip = fields.Char(related="pos_config_id.any_printer_ip", readonly=False)
+    any_printer_port = fields.Integer(related="pos_config_id.any_printer_port", readonly=False)
